38aee8e9a2b134a4e9198d0804123a279f945d29
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2018-02-19 13:13:26 +0100

    Forgot to add lwiperf_example.c to Filelists.mk

--- Filelists.mk
+++ Filelists.mk
@@ -43,6 +43,7 @@ CONTRIBAPPFILES=$(CONTRIBDIR)/apps/httpserver/httpserver-netconn.c \
 	$(CONTRIBDIR)/apps/rtp/rtp.c \
 	$(CONTRIBDIR)/examples/httpd/fs_example/fs_example.c \
 	$(CONTRIBDIR)/examples/httpd/ssi_example/ssi_example.c \
+	$(CONTRIBDIR)/examples/lwiperf/lwiperf_example.c \
 	$(CONTRIBDIR)/examples/mdns/mdns_example.c \
 	$(CONTRIBDIR)/examples/mqtt/mqtt_example.c \
 	$(CONTRIBDIR)/examples/ppp/pppos_example.c \
