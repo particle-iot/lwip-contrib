545bb632a16119d3123d238600eb82afdbffdcc5
Dirk Ziegelmeier <dziegelmeier@de.pepperl-fuchs.com>
2018-09-12 09:26:45 +0200

    iteropts.sh: Create separate build log for each iteration

--- ports/unix/example_app/iteropts.sh
+++ ports/unix/example_app/iteropts.sh
@@ -5,16 +5,18 @@ EXAPPDIR=../../../examples/example_app
 
 pushd `dirname "$0"`
 pwd
-make clean
 echo Starting Iteropts run >> $LOGFILE
 for f in $EXAPPDIR/test_configs/*.h
 do
+    echo Cleaning...
+    make clean
+    BUILDLOG=$(basename "$f" ".h").log
     echo testing $f
     echo testing $f >> $LOGFILE
     rm $EXAPPDIR/lwipopts_test.h
     # cat the file to update its timestamp
     cat $f > $EXAPPDIR/lwipopts_test.h
-    make TESTFLAGS=-DLWIP_OPTTEST_FILE -j || echo file $f failed >> $LOGFILE
+    make TESTFLAGS=-DLWIP_OPTTEST_FILE -j 8 &> $BUILDLOG 2>&1 || echo file $f failed >> $LOGFILE
     echo test $f done >> $LOGFILE
 done
 echo done, cleaning
